b9879d74c571209c81893a2162451b460a593908
mmwathe <mmwathe@andrew.cmu.edu>
2024-02-13 14:42:21 -0500

    removes standard markdown from install.js leaves extended markdown

--- src/install.js
+++ src/install.js
@@ -492,7 +492,6 @@ async function enableDefaultPlugins() {
 
     let defaultEnabled = [
         'nodebb-plugin-composer-default',
-        'nodebb-plugin-markdown',
         'nodebb-plugin-extended-markdown',
         'nodebb-plugin-mentions',
         'nodebb-widget-essentials',
