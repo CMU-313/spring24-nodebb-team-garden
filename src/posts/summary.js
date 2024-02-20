9a0a42b5ab189fa812556cefdf9abf5738c51c1a
mmwathe <mmwathe@andrew.cmu.edu>
2024-02-20 17:29:47 -0500

    attempts linting fixes

--- src/posts/summary.js
+++ src/posts/summary.js
@@ -45,9 +45,9 @@ module.exports = function (Posts) {
                 post.uid = 0;
             }
             if (!post.anonymous && post.uid === 0) {
-                post.user = uidToUser[post.uid];
+                post.user = uidToUser[post.uid]; 
             }
-            else { post.user = "Anonymous" }
+            else { post.user = 'Anonymous'; }
             Posts.overrideGuestHandle(post, post.handle);
             post.handle = undefined;
             post.topic = tidToTopic[post.tid];
