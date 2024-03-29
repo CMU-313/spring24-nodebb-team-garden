ceb05ed1e9817d74037dd0163613d96874b91197
Karissa <kdunkerl@andrew.cmu.edu>
2024-03-29 19:02:11 -0400

    updates create js and ts files to match translate pr

--- src/posts/create.js
+++ src/posts/create.js
@@ -19,7 +19,7 @@ const topics = require("../topics");
 const categories = require("../categories");
 const groups = require("../groups");
 const utils = require("../utils");
-const translate = require('../translate');
+const translate = require("../translate");
 module.exports = function (Posts) {
     function addReplyTo(postData, timestamp) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -41,6 +41,7 @@ module.exports = function (Posts) {
         const content = data.content.toString();
         const timestamp = data.timestamp || Date.now();
         const isMain = data.isMain || false;
+        const [isEnglish, translatedContent] = yield translate.translate(data);
         if (!uid && parseInt(uid, 10) !== 0) {
             throw new Error('[[error:invalid-uid]]');
         }
